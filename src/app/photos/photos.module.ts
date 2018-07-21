38020095caf7a660fae7fbe9efc8ee25e7fe4122
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-07-21 12:08:02 -0300

    Criação do componente photo dentro do módulo photos

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "../../../node_modules/@angular/core";
+
+import { PhotoComponent } from "./photo/photo.component";
+
+@NgModule({
+    declarations: [
+        PhotoComponent
+    ], 
+    exports: [
+        PhotoComponent
+    ]
+})
+export class PhotosModule {
+
+}
