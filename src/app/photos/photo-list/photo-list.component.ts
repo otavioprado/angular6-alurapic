3a7da6f8e29d2f733bf6590fc2fae467df98fbe2
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-07-29 15:46:31 -0300

    Utilizando debounce para aguardar 300ms antes de realizar novamente a operação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs/internal/Subject';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,16 +10,27 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [  ];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {  }
 
   ngOnInit(): void {
-    // pega o photos configurado como resolver no app.routing.module.ts
+    // pega o photos configurado como resolver no app.routing.module.ts e atribui à propriedade photos
     this.photos = this.activatedRoute.snapshot.data.photos;
+
+    // usando debounce é possível aguardar 300ms antes de realizar novamente a operação
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    // quando sair do photo list component vai destruir o debounce
+    this.debounce.unsubscribe();
   }
 
 }
