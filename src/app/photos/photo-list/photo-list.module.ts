4984e10e114d77583ecb9008dc436bc9d6d963b9
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-08-12 10:12:11 -0300

    criação de sub-módulos para organizar os components

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { NgModule } from '@angular/core';
+
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotoModule } from './../photo/photo.module';
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescription
+    ],
+    imports: [
+        CommonModule,
+        PhotoModule
+    ]
+})
+export class PhotoListModule {
+
+}
