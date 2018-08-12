e4d683cc1f35895e51011fa4335e011fd7222f6a
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-08-12 09:43:32 -0300

    criação do load button

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,3 +1,4 @@
+import { PhotoService } from './../photo/photo.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs/internal/Subject';
@@ -15,10 +16,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [  ];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {  }
+  constructor(private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService) {  }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     // pega o photos configurado como resolver no app.routing.module.ts e atribui à propriedade photos
     this.photos = this.activatedRoute.snapshot.data.photos;
 
@@ -31,6 +37,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     // quando sair do photo list component vai destruir o debounce
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+      if(!photos.length) {
+        this.hasMore = false;
+      }
+    });
   }
 
 }
